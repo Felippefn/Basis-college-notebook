252549095dba7f3ce26a628d1c6d90e483398240
Felippe Nagy <79763393+Felippefn@users.noreply.github.com>
2024-09-07 01:35:55 -0400

    :rocket: added new icons to home page and changed colors for the main app

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/" className='title-nav'>College Notebook</Navbar.Brand>
+    <Navbar style={{ backgroundColor: '#0B090A' }} variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" className='title-nav'>Basis</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,47 @@
 import React from 'react';
+import { GrAdd } from "react-icons/gr";
+import { GrSchedules } from "react-icons/gr";
+import { GrCalendar } from "react-icons/gr";
+import { GrList } from "react-icons/gr";
+
+function Hello(){
+  const a = "Hello World!"
+  
+  return a
+}
 
 function Home() {
   return (
     <div>
       <h1 className='title-page'>Welcome!</h1>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            Column
-          </div>
-          <div class="col">
-            Column
-          </div>
-          <div class="col">
-            Column
+      <div className="container">
+        <div className="row justify-content-center mb-5 mt-0">
+          <div className="alert alert-secondary alert-dismissible fade show col-3 text-center" role="alert">
+            <strong>{Hello()}</strong> You should check in on some of those fields below.
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
         </div>
       </div>
-      {/* Add your home page content here */}
+      <div className="container icons-home">
+        <div className="row">
+          <div className="col">
+            <GrAdd fontSize={"100px"} />
+            <p>Add Notes</p>
+          </div>
+          <div className="col">
+            <GrSchedules fontSize={"100px"} />
+            <p>Check Schedule</p>
+          </div>
+          <div className="col">
+            <GrCalendar fontSize={"100px"} />
+            <p>Events</p>
+          </div>
+          <div className="col">
+            <GrList fontSize={"100px"} />
+            <p>To-Do List</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
